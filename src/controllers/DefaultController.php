47c5ef07099580eca5636dafba65cc83f70c1615
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2020-10-13 12:00:51 +0200

    added settings for all attributes

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -118,15 +118,19 @@ class DefaultController extends Controller
 
     public function actions()
     {
+        $testLimit = Yii::$app->settings->getOrSet('testLimit', 100, 'captcha', 'integer');
+        $width = Yii::$app->settings->getOrSet('width', 140, 'captcha', 'integer');
+        $height = Yii::$app->settings->getOrSet('height', 75, 'captcha', 'integer');
+        $offset = Yii::$app->settings->getOrSet('offset', -1, 'captcha', 'integer');
         $backColor = Yii::$app->settings->getOrSet('backColor', '0x333333', 'captcha', 'string');
         $foreColor = Yii::$app->settings->getOrSet('foreColor', '0xFFFFFF', 'captcha', 'string');
         $actions = parent::actions();
         $actions['captcha'] = [
             'class' => CaptchaAction::class,
-            'testLimit' => 100,
-            'width' => 140,
-            'height' => 75,
-            'offset' => -1,
+            'testLimit' => $testLimit,
+            'width' => $width,
+            'height' => $height,
+            'offset' => $offset,
             'backColor' =>  hexdec($backColor),
             'foreColor' => hexdec($foreColor)
         ];
