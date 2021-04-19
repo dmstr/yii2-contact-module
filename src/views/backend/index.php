2c6e4358e90256424277669a4fc27ba7e714a397
Tobias Munk <schmunk@usrbin.de>
2021-04-19 15:00:01 +0200

    added translatables

--- src/views/backend/index.php
+++ src/views/backend/index.php
@@ -1,17 +1,20 @@
 <?php
 /* @var $this yii\web\View */
+
 use yii\helpers\Html;
+use Yii;
+
 ?>
-<h1>Overview</h1>
+<h1><?= Yii::t('contact', 'Overview') ?></h1>
 
 <p>
-  <?= Html::a('Templates', ['crud/contact-template']) ?>
+    <?= Html::a(Yii::t('contact', 'Templates'), ['crud/contact-template']) ?>
 </p>
 
 <p>
-    <?= Html::a('Logs', ['crud/contact-log']) ?>
+    <?= Html::a(Yii::t('contact', 'Logs'), ['crud/contact-log']) ?>
 </p>
 
 <p>
-    <?= Html::a('Export', ['export/index']) ?>
+    <?= Html::a(Yii::t('contact', 'Export'), ['export/index']) ?>
 </p>
