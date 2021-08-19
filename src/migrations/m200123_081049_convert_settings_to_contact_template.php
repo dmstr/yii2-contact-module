5d70439d6d151e7cb1e2b0c39942ab7d38a7ea8e
Elias Luhr <e.luhr@herzogkommunikation.de>
2021-08-19 15:32:53 +0200

    Added support for postgres db

--- src/migrations/m200123_081049_convert_settings_to_contact_template.php
+++ src/migrations/m200123_081049_convert_settings_to_contact_template.php
@@ -15,57 +15,60 @@ class m200123_081049_convert_settings_to_contact_template extends Migration
      */
     public function safeUp()
     {
-        $schemaRows = (new Query())
-            ->select(['name' => new Expression('SUBSTR(`key`, 1, LOCATE(".", `key`) - 1)')])
-            ->from('{{%settings}}')
-            ->where(['section' => 'contact'])
-            ->groupBy('name')
-            ->all();
 
-        $schemaNames = ArrayHelper::getColumn($schemaRows, 'name');
-
-
-        foreach ($schemaNames as $schemaName) {
-            $settingRows = (new Query())
-                ->select([
-                    'value',
-                    'identifier' => new Expression('SUBSTR(`key`, LOCATE(".", `key`) + 1)'),
-                    'id'
-                ])
+        if ($this->db->getDriverName() === 'mysql') {
+            $schemaRows = (new Query())
+                ->select(['name' => new Expression('SUBSTR([[key]], 1, LOCATE(".", [[key]]) - 1)')])
                 ->from('{{%settings}}')
                 ->where(['section' => 'contact'])
-                ->andWhere("`key` LIKE '{$schemaName}.%'")
+                ->groupBy('name')
                 ->all();
 
-            $columns = [
-                'name' => $schemaName,
-                'created_at' => new Expression('NOW()'),
-                'updated_at' => new Expression('NOW()')
-            ];
+            $schemaNames = ArrayHelper::getColumn($schemaRows, 'name');
 
-            $settingsIds = [];
 
-            foreach ($settingRows as $settingRow) {
-                $value = $settingRow['value'];
-                switch ($settingRow['identifier']) {
-                    case 'schema':
-                        $columns['form_schema'] = $value;
-                        break;
-                    case 'toEmail':
-                        $columns['to_email'] = $value;
-                        break;
-                    case 'fromEmail':
-                        $columns['from_email'] = $value;
-                        break;
-                    case 'subject':
-                        $columns['email_subject'] = $value;
-                        break;
+            foreach ($schemaNames as $schemaName) {
+                $settingRows = (new Query())
+                    ->select([
+                        'value',
+                        'identifier' => new Expression('SUBSTR([[key]], LOCATE(".", [[key]]) + 1)'),
+                        'id'
+                    ])
+                    ->from('{{%settings}}')
+                    ->where(['section' => 'contact'])
+                    ->andWhere("[[key]] LIKE '{$schemaName}.%'")
+                    ->all();
+
+                $columns = [
+                    'name' => $schemaName,
+                    'created_at' => new Expression('NOW()'),
+                    'updated_at' => new Expression('NOW()')
+                ];
+
+                $settingsIds = [];
+
+                foreach ($settingRows as $settingRow) {
+                    $value = $settingRow['value'];
+                    switch ($settingRow['identifier']) {
+                        case 'schema':
+                            $columns['form_schema'] = $value;
+                            break;
+                        case 'toEmail':
+                            $columns['to_email'] = $value;
+                            break;
+                        case 'fromEmail':
+                            $columns['from_email'] = $value;
+                            break;
+                        case 'subject':
+                            $columns['email_subject'] = $value;
+                            break;
+                    }
+                    $settingsIds[] = $settingRow['id'];
                 }
-                $settingsIds[] = $settingRow['id'];
-            }
 
-            $this->insert('{{%dmstr_contact_template}}', $columns);
-            $this->delete('{{%settings}}', ['id' => $settingsIds]);
+                $this->insert('{{%dmstr_contact_template}}', $columns);
+                $this->delete('{{%settings}}', ['id' => $settingsIds]);
+            }
         }
     }
 
