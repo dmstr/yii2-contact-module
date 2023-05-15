dcb206c2451c7988e91ad2126323ac227b8fff9a
Tobias Munk <schmunk@usrbin.de>
2023-05-15 12:34:43 +0200

    added folder param with runtime alias

--- src/views/export/index.php
+++ src/views/export/index.php
@@ -34,6 +34,7 @@ use yii\i18n\Formatter;
         echo ExportMenu::widget(
             [
                 'dataProvider' => $dataProvider,
+                'folder' => '@runtime/contact-export',
                 'columns' => $columns,
                 'formatter' => ['class' => Formatter::class, 'nullDisplay' => ''],
                 'exportConfig' => [
